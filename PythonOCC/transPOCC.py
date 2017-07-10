6fe172135ee75f60ec4fac0d32eb74422eefe6be
Julia Li <miaonaise@users.noreply.github.com>
2017-07-10 16:57:41 +0200

    Update transPOCC.py

--- PythonOCC/transPOCC.py
+++ PythonOCC/transPOCC.py
@@ -1,19 +1,47 @@
 
 from OCC.gp import *
 from OCC.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeCylinder
-
+from OCC.BRepAlgoAPI import BRepAlgoAPI_Cut, BRepAlgoAPI_Fuse 
 from OCC.STEPControl import STEPControl_Writer, STEPControl_AsIs
 
 
-# creates a basic shape
-box_s = BRepPrimAPI_MakeBox(10, 20, 30).Shape()
-p = gp_Ax2(gp_Pnt(0,0,30),gp_DZ())
-cyl_s = BRepPrimAPI_MakeCylinder(p,2,50).Shape()
+# TANK
+TL = 20; TW = 60; TH = 30
+tank = BRepPrimAPI_MakeBox(TL, TW, TH).Shape()
+
+# BUSHING
+BH = 40 # bush height/total length
+BHL = 4 # head length
+BTL = 5 # tap length
+BCL = BH-BHL-BTL # core length
+BOR = 3 # outer radius
+BCR = 2.7 # core radius
+
+BIRSm = 3.5 # radius of small CI
+BIHSm = 0.5 # height of small CI
+BIRLg = 4 # radius of large CI
+BIHLg = float(BIRLg*BIHSm)/BIRSm # height of large CI (proportional)
+BIS = 0.5 # seperation between CIs
+
+BIHPack = BIHSm+BIHLg+2*BIS # pack length
+BIno = int(BCL//BIHPack) # number of packs fitted into core length
+BIrem = BCL % BIHPack # remainder length
+
+Btap = BRepPrimAPI_MakeCylinder(BOR,BTL)
+p = gp_Ax2(gp_Pnt(0,0,BTL),gp_DZ()) # placement
+Bcore = BRepPrimAPI_MakeCylinder(p,BCR,BCL)
+p = gp_Ax2(gp_Pnt(0,0,BTL+BCL),gp_DZ()) # placement
+Bhead = BRepPrimAPI_MakeCylinder(p,BOR,BHL)
+bushing = BRepAlgoAPI_Fuse(Btap, Bcore).Shape()
+bushing = BRepAlgoAPI_Fuse(bushig, Bhead).Shape()
+
+
+
 
 # initialize the STEP exporter
 step_writer = STEPControl_Writer()
 
 # transfer shapes and write file
-step_writer.Transfer(box_s,STEPControl_AsIs)
+step_writer.Transfer(tank,STEPControl_AsIs)
 step_writer.Transfer(cyl_s,STEPControl_AsIs)
 step_writer.Write("test.stp")
