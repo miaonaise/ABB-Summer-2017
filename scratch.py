c17e6af6e7fe3c70c9a7e184393f9ece65288243
Julia Li <miaonaise@users.noreply.github.com>
2017-07-11 22:56:58 +0200

    Create scratch.py

---
+++ scratch.py
@@ -0,0 +1,12 @@
+import Draft
+
+v1 = Base.Vector(0,radius,0)
+v2 = Base.Vector(0,radius+body_width,0)
+v3 = Base.Vector(0,radius+body_width,body_height)
+v4 = Base.Vector(0,radius,body_height)
+wire = Draft.makeWire([v1,v2,v3,v4],closed=True)
+surface = Part.Face(wire)
+
+decoy = Draft.makeWire([v1,v2,v3,v4],closed=True)
+wire = decoy.Shape
+surface = Part.Face(wire)
