7d486b19405399ded5b708b00a24a6cd9d253624
Julia Li <miaonaise@users.noreply.github.com>
2017-07-11 09:38:35 +0200

    Update transPOCC.py

--- PythonOCC/transPOCC.py
+++ PythonOCC/transPOCC.py
@@ -3,7 +3,7 @@ from OCC.gp import *
 from OCC.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeCylinder, BRepPrimAPI_MakeCone
 from OCC.BRepAlgoAPI import BRepAlgoAPI_Cut, BRepAlgoAPI_Fuse 
 from OCC.STEPControl import STEPControl_Writer, STEPControl_AsIs
-
+import math
 
 # TANK
 TL = 20; TW = 60; TH = 30
