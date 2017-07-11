d883b59a7fd060f6d224f6e631b20bbb7a8d4dde
Julia Li <miaonaise@users.noreply.github.com>
2017-07-11 23:34:47 +0200

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,17 +1,15 @@
-import Draft
 
-v1 = Base.Vector(0,radius,0)
-v2 = Base.Vector(0,radius+body_width,0)
-v3 = Base.Vector(0,radius+body_width,body_height)
-v4 = Base.Vector(0,radius,body_height)
-wire = Draft.makeWire([v1,v2,v3,v4],closed=True)
+vertices = [(0,radius,0),(0,radius+body_width,0),(0,radius+body_width,body_height),(0,radius,body_height),(0,radius,0)]
+
+edges = []
+for i in range(4):
+  edge = Part.makeLine(vertices[i],vertices[i+1])
+  edges.append(edge)
+
+wire = Part.Wire(edges)
 surface = Part.Face(wire)
 
-decoy = Draft.makeWire([v1,v2,v3,v4],closed=True)
-wire = decoy.Shape
-surface = Part.Face(wire)
 
-edge1 = Part.Line(v1,v2)
 
 
 https://github.com/tpaviot/pythonocc-core/blob/master/examples/core_topology_revolved_shape.py
