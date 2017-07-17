d344f7bf2be64cff77d13552ebec82014b381db4
Julia Li <miaonaise@users.noreply.github.com>
2017-07-17 15:33:16 +0200

    Update section.py

--- PythonOCC/section.py
+++ PythonOCC/section.py
@@ -61,10 +61,7 @@ ltrsf.SetTranslation(gp_Vec(BIn+BOR,float(TW)/2-BS-BCOS,TH-BSIN))
 leftBush = BRepBuilderAPI_Transform(leftBush, ltrsf).Shape()
 leftBush = BRepAlgoAPI_Cut(leftBush,tank).Shape() # cut common part of bushing and tank
 
-meh = BRepAlgoAPI_Section(leftBush,tank)
-meh.Approximation(True)
-meh.Build()
-hoho = meh.Shape()
+meh = BRepAlgoAPI_Section(leftBush,tank).Shape()
 leftBush = BRepAlgoAPI_Cut(leftBush,hoho).Shape()
 
 mtrsf = gp_Trsf() # create and set up transformation for midBush
