84957a944f0b4aa566e291797b4e770fdce3ad98
Julia Li <miaonaise@users.noreply.github.com>
2017-07-19 09:13:08 +0200

    Update transformerFreeCAD.py

--- FreeCAD/transformerFreeCAD.py
+++ FreeCAD/transformerFreeCAD.py
@@ -124,4 +124,4 @@ __objs__.append(FreeCAD.getDocument("transformer").getObject("leftFan"))
 __objs__.append(FreeCAD.getDocument("transformer").getObject("rightFan"))
 
 # filetypes: step, iges, stl
-Part.export(__objs__,"transformerFreeCAD.iges")
+Part.export(__objs__,"transformerFreeCAD.stl")
